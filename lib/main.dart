5dbdfc6a0721d5972a13e3f8e90ecf11052dc473
ismail bentabet <xisben2001x@gmail.com>
2020-11-26 13:00:39 +0100

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import './Widgets/gender.dart';
-import './Widgets/height.dart';
-import './Widgets/age_weight.dart';
 import './routes/result.dart';
 
 void main() => runApp(MyApp());
@@ -23,6 +20,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+    double height = 20;
+
+  double age = 0;
+  double weight = 0;
+
+  void addit(double par) {
+    par++;
+  }
+
+  void minit(double par) {
+    setState(() {
+      par--;
+    });
+  }
+
+  double gender = null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,26 +45,250 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Gender(),
-              Heightwid(),
-              AgeWeight(),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              gender = 0;
+                              ;
+                            });
+                          },
+                          child: Card(
+                            child: Container(
+                                color: gender == 0
+                                    ? Color(0xff41445c)
+                                    : Color(0xff1D1F33),
+                                height: 120.0,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.face,
+                                      color: Colors.blue,
+                                      size: 36.0,
+                                    ),
+                                    Text(
+                                      'Male',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                          )),
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              gender = 1;
+                              ;
+                            });
+                          },
+                          child: Card(
+                            child: Container(
+                                color: gender == 1
+                                    ? Color(0xff41445c)
+                                    : Color(0xff1D1F33),
+                                height: 120.0,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.face,
+                                      color: Colors.blue,
+                                      size: 36.0,
+                                    ),
+                                    Text(
+                                      'Female',
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                          )),
+                    ),
+                  ]),
+              Row(children: <Widget>[
+                Expanded(
+                  child: Card(
+                    child: Container(
+                        color: Color(0xff1D1F33),
+                        height: 120.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(height: 10),
+                            Text(
+                              'HEIGHT',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text('$height CM'),
+                            Slider(
+                              value: height,
+                              min: 0,
+                              max: 300,
+                              divisions: 20,
+                              label: height.round().toString(),
+                              onChanged: (double value) {
+                                setState(() {
+                                  height = value;
+                                });
+                              },
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+              ]),
+              Row(children: <Widget>[
+                Expanded(
+                  child: Card(
+                    child: Container(
+                        color: Color(0xff1D1F33),
+                        height: 120.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Age',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text('$age'),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  MaterialButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        age++;
+                                      });
+                                    },
+                                    color: Colors.blue,
+                                    textColor: Colors.white,
+                                    child: Icon(
+                                      Icons.add,
+                                      size: 24,
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    shape: CircleBorder(),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        if (age > 0) {
+                                          age--;
+                                        }
+                                      });
+                                    },
+                                    color: Colors.blue,
+                                    textColor: Colors.white,
+                                    child: Icon(
+                                      Icons.remove,
+                                      size: 24,
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    shape: CircleBorder(),
+                                  )
+                                ])
+                          ],
+                        )),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                        color: Color(0xff1D1F33),
+                        height: 120.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Weight',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text('${weight}'),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  MaterialButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        weight++;
+                                      });
+                                    },
+                                    color: Colors.blue,
+                                    textColor: Colors.white,
+                                    child: Icon(
+                                      Icons.add,
+                                      size: 24,
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    shape: CircleBorder(),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        if (weight > 0) {
+                                          weight--;
+                                        }
+                                      });
+                                    },
+                                    color: Colors.blue,
+                                    textColor: Colors.white,
+                                    child: Icon(
+                                      Icons.remove,
+                                      size: 24,
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    shape: CircleBorder(),
+                                  )
+                                ])
+                          ],
+                        )),
+                  ),
+                ),
+              ]),
+              
               Card(
                   color: Color(0xff1D1F33),
                   child: Container(
                       width: double.infinity,
                       child: FlatButton(
                           child: Text('Claculate Your BM'),
-                          
                           onPressed: () {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => ResultRoute()),
-  );
-}
-                          
-                          
-                          
-                          ))),
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ResultRoute(gender,height,age,weight)),
+                            );
+                          }))),
             ]));
     ;
   }
--- lib/routes/result.dart
+++ lib/routes/result.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 import '../Widgets/result_class.dart';
-
+import 'dart:math';
 
 
 List <ResultWid> resultList = [
 ResultWid(category:'Underweight	',letter:'ughhh eat something , fucking sandwitch'),
 ResultWid(category:'Normal',letter:'you doing fine keep it up'),
 ResultWid(category:'Overweight',letter:'really ??? what you gonna next , me?'),
+
 ];
 
-class ResultRoute extends StatelessWidget {
+
+
+
+class ResultRoute extends StatefulWidget {
+final double age;
+  final double weight;
+  final double height;
+  final double gender;
+  ResultRoute(this.gender, this.height ,this.age, this.weight);
+  @override
+  _ResultRouteState createState() => _ResultRouteState();
+}
+
+class _ResultRouteState extends State<ResultRoute> {
+
   @override
   Widget build(BuildContext context) {
+      double  result = widget.weight / pow (widget.height/100,2);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Result"),
+        title: Text('Result'),
       ),
       body: Center(
         child: Card(
@@ -26,8 +42,13 @@ class ResultRoute extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                           crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('Normal'),
-                Text('22.1'),
+            Text('normal'),
+                Text(result.round().toString(),
+                style:TextStyle(
+                    color:Colors.green,
+                    fontSize : 40,
+                )
+                ),
                   Text('Normal Bmi Ranege'),
                 Text('You Are Doing Great'),
               ],
@@ -35,6 +56,6 @@ class ResultRoute extends StatelessWidget {
           ),
         ),
       ),
-    );
+    );;
   }
 }
